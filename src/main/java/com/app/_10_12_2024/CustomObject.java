ce7ab66185896495048b1fb6b8c0f1a562312abe
shonmikl <mikhailegorov676@gmail.com>
2024-12-10 12:59:32 +0100

    taski.txt

--- src/main/java/com/app/_10_12_2024/CustomObject.java
+++ src/main/java/com/app/_10_12_2024/CustomObject.java
@@ -2,19 +2,8 @@ package com.app._10_12_2024;
 
 public class CustomObject {
 
-
-
-
-
-
     public CustomObject[] generate() {
         CustomObject[] customObjects = new CustomObject[20];
-
-
-
-
-
-
         return customObjects;
     }
 
