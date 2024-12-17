8b212f23b2c907ec2c96c36c145d4493eb136642
shonmikl <mikhailegorov676@gmail.com>
2024-12-17 10:26:44 +0100

    methods. getter, setter

--- src/main/java/com/app/_17_12_2024/Animal.java
+++ src/main/java/com/app/_17_12_2024/Animal.java
@@ -1,5 +1,8 @@
 package com.app._17_12_2024;
 
+import lombok.Getter;
+
+@Getter
 public class Animal {
     private String name;
     private int age;
@@ -9,20 +12,27 @@ public class Animal {
         this.name = name;
         this.age = age;
     }
+
+    public void makeVoice() {
+        System.out.println("ANIMAL MAKE NOISE");
+    }
 }
 
 class Person22 {
     public static void main(String[] args) {
-        Cat cat = new Cat("name", 1);
 
     }
-
 }
 
 class Cat extends Animal {
     public Cat(String name, int age) {
         System.out.println("CAT");
         super(name, age);
+    }
+
+    @Override
+    public void makeVoice() {
+        System.out.println("CAT MAKE NOISE");
     }
 }
 
