588dd15ff5f7e6d530916235f55808f9d23cf3b9
shonmikl <mikhailegorov676@gmail.com>
2024-12-18 23:25:42 +0100

    NY tasks

---
+++ src/main/java/com/app/_19_12_2024/T3.java
@@ -0,0 +1,94 @@
+package com.app._19_12_2024;
+
+public class T3 {
+
+    /**
+     * 1. Напишите метод, который принимает массив целых чисел и возвращает его длину.
+     */
+    public static int getArrayLength(int[] array) {
+        // TODO: реализовать метод
+        return 0;
+    }
+
+    /**
+     * 2. Напишите метод, который принимает массив целых чисел и выводит его элементы в консоль.
+     */
+    public static void printArray(int[] array) {
+        // TODO: реализовать метод
+    }
+
+    /**
+     * 3. Напишите метод, который принимает массив целых чисел и возвращает сумму его элементов.
+     */
+    public static int sumArrayElements(int[] array) {
+        // TODO: реализовать метод
+        return 0;
+    }
+
+    /**
+     * 4. Напишите метод, который принимает массив целых чисел и возвращает максимальный элемент.
+     */
+    public static int findMaxElement(int[] array) {
+        // TODO: реализовать метод
+        return 0;
+    }
+
+    /**
+     * 5. Напишите метод, который принимает массив целых чисел и возвращает минимальный элемент.
+     */
+    public static int findMinElement(int[] array) {
+        // TODO: реализовать метод
+        return 0;
+    }
+
+    /**
+     * 6. Напишите метод, который принимает массив целых чисел и возвращает
+     * новый массив, содержащий только чётные элементы.
+     */
+    public static int[] filterEvenNumbers(int[] array) {
+        // TODO: реализовать метод
+        return new int[0];
+    }
+
+    /**
+     * 7. Напишите метод, который принимает массив целых чисел и
+     * возвращает массив в обратном порядке.
+     */
+    public static int[] reverseArray(int[] array) {
+        // TODO: реализовать метод
+        return new int[0];
+    }
+
+    /**
+     * 8. Напишите метод, который принимает массив целых чисел и возвращает true,
+     * если массив отсортирован по возрастанию.
+     */
+    public static boolean isArraySorted(int[] array) {
+        // TODO: реализовать метод
+        return false;
+    }
+
+    /**
+     * 9. Напишите метод, который принимает массив целых чисел и возвращает
+     * среднее арифметическое его элементов.
+     */
+    public static double calculateAverage(int[] array) {
+        // TODO: реализовать метод
+        return 0.0;
+    }
+
+    /**
+     * 10. Напишите метод, который принимает массив целых чисел и
+     * возвращает количество чётных чисел в массиве.
+     */
+    public static int countEvenNumbers(int[] array) {
+        // TODO: реализовать метод
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        // Пример вызовов методов (после их реализации)
+        // int[] exampleArray = {1, 2, 3, 4, 5};
+        // System.out.println(getArrayLength(exampleArray));
+    }
+}
